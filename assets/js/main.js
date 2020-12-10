df517e8dd51b78a7dbe912bfbce0c24511710462
YaniMad <yani.madebos@mmibordeaux.comm>
2020-12-10 16:53:40 +0100

    slide comments in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,15 @@ var slider = tns({
     nav: false,
     controls: false
 });
+
+var slider = tns({
+    container: '.slider-comments',
+    items: 1,
+    center: true,
+    loop: true,
+    swipeAngle: false,
+    mouseDrag: true,
+    speed: 350,
+    nav: true,
+    controls: false,
+});
