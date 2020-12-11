b484487a22c69222cb3988af26cb3fb3338e4de2
Triips-TheCoder <julian.laballe@mmibordeaux.com>
2020-12-11 02:15:32 +0100

    deux heures du matin sa dort pas mon pote là j'ai quasiment tout fait les pop up sont tous la mais mtn faut voir comment les faire apparaitre en js j'continue le taff la bise

---
+++ assets/js/pop-up.js
@@ -0,0 +1,8 @@
+/* const boutton = document.querySelectorAll('#btn');
+const popUp = document.querySelector('.pop-up display');
+
+boutton.addEventListener("click", function () {
+    popUp.classList.remove('display');
+
+}
+) */
